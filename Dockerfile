347594586b8b8d7a45f744aec9705894cd547b04
John J. Chaverra <jjchaverra@gmail.com>
2023-03-28 14:16:00 -0500

    Prueba Yeferson

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM python:3.7-alpine
-
-RUN pip install --quiet --no-cache-dir awscli
-
-ADD entrypoint.sh /entrypoint.sh
-RUN chmod +x /entrypoint.sh
-ENTRYPOINT [ "/entrypoint.sh" ]
--- scripts/pipeline/release.sh
+++ scripts/pipeline/release.sh
@@ -4,14 +4,9 @@ aws_access_key=$3
 aws_access_secret=$4
 local_path=$5
 
-# Remove any existing versions of a ZIP
-#rm -rf $local_path
 
-# Create a zip of the current directory.
-# zip -r $local_path . -x .git/ .git/*** .github/workflows/release.yml scripts/pipeline/release.sh scripts/pipeline/upload_file_to_s3.py .DS_Store
-
-# Install required dependencies for Python script.
+# Instala boto3
 pip3 install boto3
 
-# Run upload script
+# Run script
 python3 scripts/pipeline/upload_file_to_s3.py $bucket_name $aws_key $aws_access_key $aws_access_secret $local_path
